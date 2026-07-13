0838b8176f405f01de267378beda58fd0e870b55
Anshu Panthi <anshupanthi01@gmail.com>
2026-07-13 23:02:26 +0545

    Repository

---
+++ app/users/repository.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.users.model import User
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session=session
+
+    async def find_by_id(self, user_id: int)->Optional[User]:
+        result = await self.session.execute(select(User).where(User.id == user_id))
+        return result.scalar_one_or_none()
+    
+    async def find_by_username(self, username: str)-> Optional[User]:
+        result = await self.session.execute(select(User).where(User.username == username))
+        return result.scalar_one_or_none()
+    
+    async def find_by_email(self, email: str)->Optional[User]:
+        result = await self.session.execute(select(User).where(User.email == email))
+        return result.scalar_one_or_none()
+    
+    def create(self, user: User)-> User:
+        self.session.add(user)
+        return user
