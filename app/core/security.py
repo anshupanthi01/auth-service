57b6eb258c9ea83d4d264e6e9db2b18834ed85a9
Anshu Panthi <anshupanthi01@gmail.com>
2026-07-19 21:16:09 +0545

    security

---
+++ app/core/security.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta, timezone
+from authlib.jose import jwt, JoseError, InvalidTokenError
+from app.core.config import settings as s
+from typing import Any
+from passlib.context import CryptContext
+
+
+
+def create_access_token( data: dict[str, Any], expires_delta: timedelta | None = None ) -> str:
+    payload = data.copy()
+
+    expire = datetime.now(timezone.utc) + (
+        expires_delta
+        if expires_delta
+        else timedelta(minutes= s.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    payload.update(
+        {
+            "exp": expire,
+            "iat": datetime.now(timezone.utc),
+        }
+    )
+
+    return jwt.encode(
+        algorithm= s.algorithm,
+        payload= payload,
+        key= s.SECRET_KEY.get_secret_value()
+    )
+
+
+def decode_token(token: str) -> dict[str, Any]:
+    try:
+        claims = jwt.decode(
+        token, 
+        s.SECRET_KEY.get_secret_value())
+        claims.validate()
+        return claims
+    except JoseError:
+        raise InvalidTokenError(
+            description= "Invalid or expired token"
+        )
+
+
