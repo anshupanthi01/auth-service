0cbfc9f201a058b17971b32adb0630edae04738f
Anshu Panthi <anshupanthi01@gmail.com>
2026-08-04 22:36:25 +0545

    m

--- app/auth/repository.py
+++ app/auth/repository.py
@@ -1,11 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.users.model import RefreshToken
 
 class RefreshTokenRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create():
-        pass
+    async def create(self, refresh_token: RefreshToken)->RefreshToken:
+        self.session.add(refresh_token)
+        return refresh_token
 
     async def find_by_hash():
         pass
