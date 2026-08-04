5c596ceb0cf03a50af12ea4a116d39adb812fe39
Anshu Panthi <anshupanthi01@gmail.com>
2026-08-04 22:43:51 +0545

    m

--- app/auth/repository.py
+++ app/auth/repository.py
@@ -5,16 +5,22 @@ class RefreshTokenRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create(self, refresh_token: RefreshToken)->RefreshToken:
-        self.session.add(refresh_token)
-        return refresh_token
+    def create(self, token: RefreshToken)->RefreshToken:
+        self.session.add(token)
+        return token
 
-    async def find_by_hash():
+    async def find_by_hash(self, token_hash: str)-> RefreshToken | None:
         pass
 
-    async def revoke():
+    async def revoke(self, token: RefreshToken)-> None:
         pass
 
+    async def revoke_all_for_user(self, user_id: int)-> None:
+        pass
+
+    async def find_active_by_user():
+        pass
+    
     async def delete_expired():
         pass
 
